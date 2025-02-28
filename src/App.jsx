ddaad0d166c2362bd73e0cd412b4b5e74dc7b2b5
sophialiang <liang.yime@northeastern.com>
2025-02-27 17:46:04 -0800

    did extra credit: use context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 import Counter from "./components/Counter";
 function App() {
-  return (
-    <>
-      <Counter />
-    </>
-  );
+  return <Counter />;
 }
 
 export default App;
